80c56cd20cad90f77dbf3dbb70e1bdc4003cd8f4
Jayvee <jayveetolibas@gmail.com>
2020-06-08 12:59:28 +0800

    error on running npm run watch or yarn watch

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,22 +1,22 @@
-const path = require('path')
-const fs = require('fs-extra')
-const mix = require('laravel-mix')
-require('laravel-mix-versionhash')
+const path = require("path");
+const fs = require("fs-extra");
+const mix = require("laravel-mix");
+require("laravel-mix-versionhash");
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 mix
-  .js('resources/js/app.js', 'public/dist/js')
-  .sass('resources/sass/app.scss', 'public/dist/css')
+  .js("resources/js/app.js", "public/dist/js")
+  .sass("resources/sass/app.scss", "public/dist/css")
 
-  .disableNotifications()
+  .disableNotifications();
 
 if (mix.inProduction()) {
   mix
     // .extract() // Disabled until resolved: https://github.com/JeffreyWay/laravel-mix/issues/1889
     // .version() // Use `laravel-mix-versionhash` for the generating correct Laravel Mix manifest file.
-    .versionHash()
+    .versionHash();
 } else {
-  mix.sourceMaps()
+  mix.sourceMaps();
 }
 
 mix.webpackConfig({
@@ -24,30 +24,13 @@ mix.webpackConfig({
     // new BundleAnalyzerPlugin()
   ],
   resolve: {
-    extensions: ['.js', '.json', '.vue'],
+    extensions: [".js", ".json", ".vue"],
     alias: {
-      '~': path.join(__dirname, './resources/js')
+      "~": path.join(__dirname, "./resources/js")
     }
   },
   output: {
-    chunkFilename: 'dist/js/[chunkhash].js',
-    path: mix.config.hmr ? '/' : path.resolve(__dirname, './public/build')
+    chunkFilename: "dist/js/[chunkhash].js",
+    path: mix.config.hmr ? "/" : path.resolve(__dirname, "./public")
   }
-})
-
-mix.then(() => {
-  if (!mix.config.hmr) {
-    process.nextTick(() => publishAseets())
-  }
-})
-
-function publishAseets () {
-  const publicDir = path.resolve(__dirname, './public')
-
-  if (mix.inProduction()) {
-    fs.removeSync(path.join(publicDir, 'dist'))
-  }
-
-  fs.copySync(path.join(publicDir, 'build', 'dist'), path.join(publicDir, 'dist'))
-  fs.removeSync(path.join(publicDir, 'build'))
-}
+});
